2b51cd26763cab67e0fb0090ec8b1cedae7f895f
Mariana Moraes <mari_a.s.p@hotmail.com>
2023-01-12 22:11:08 -0300

    requisito 3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 const { readTalker } = require('./utils/fsUtils');
+const { generateToken } = require('./utils/generateToken');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -18,13 +19,22 @@ app.get('/talker', async (req, res) => {
 });
 
 app.get('/talker/:id', async (req, res) => {
-    const talker = await readTalker();
-    const talkerId = talker.find(({ id }) => id === Number(req.params.id));
+  const talker = await readTalker();
+  const talkerId = talker.find(({ id }) => id === Number(req.params.id));
 
-    if (!talkerId) {
-      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-    }
-    res.status(200).json(talkerId);
+  if (!talkerId) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talkerId);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const token = generateToken();
+
+  if (email && password) {
+    return res.status(200).json({ token: `${token}` });
+  }
 });
 
 app.listen(PORT, () => {
